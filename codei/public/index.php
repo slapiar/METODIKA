b9113b95a87d85c522ae6ac43d0ba60104f66d1a
Slavomir Piar <33904511+slapiar@users.noreply.github.com>
2026-07-22 14:25:05 +0200

    Pripojenie externého env pri webovom štarte

--- codei/public/index.php
+++ codei/public/index.php
@@ -41,19 +41,15 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
  *---------------------------------------------------------------
  * BOOTSTRAP THE APPLICATION
  *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
  */
 
-// LOAD OUR PATHS CONFIG FILE
-// This is the line that might need to be changed, depending on your folder structure.
+require FCPATH . '../app/Config/ExternalEnvironment.php';
+\Config\ExternalEnvironment::load();
+
 require FCPATH . '../app/Config/Paths.php';
-// ^^^ Change this line if you move your application folder
 
 $paths = new Paths();
 
-// LOAD THE FRAMEWORK BOOTSTRAP FILE
 require $paths->systemDirectory . '/Boot.php';
 
 exit(Boot::bootWeb($paths));
